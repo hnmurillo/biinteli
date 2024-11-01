071acea41764cf07e769bf8205565cd6845544f5
HelenMurillo <156430545+hnmurillo@users.noreply.github.com>
2024-11-01 17:00:43 -0500

    feat: :sparkles:  Backend prueba

---
+++ biiteli/src/main/java/com/example/biiteli/BiiteliApplication.java
@@ -0,0 +1,13 @@
+package com.example.biiteli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiiteliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiiteliApplication.class, args);
+	}
+
+}
